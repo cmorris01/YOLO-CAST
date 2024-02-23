3cedac0f86b7190f535d9e2a66a64fcab36eb01f
Coy Morris <crm058@uark.edu>
2024-02-23 10:18:34 -0600

    Adding documentation and created train_model.py for training model.

---
+++ train_model.py
@@ -0,0 +1,25 @@
+"""
+train_model.py
+~~~~~~~~~~~~
+
+A module to train YOLO models on UARK hpc. Functionality includes (as far as I've thought) 
+
+"""
+# %%
+# Setting environmental variable and import statements
+import os
+import torch
+from ultralytics import YOLO
+
+# Specifying cuda GPU memory allocation. Required in google colab to deal with memory overrun, using as safeguard for HPC
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:256'
+
+# Intializing GPU use if available for training.
+if torch.cuda.is_available():
+    device_name = torch.device("cuda")
+else:
+    device_name = torch.device('cpu')
+print(f"Using {device_name} for training.")
+
+# %%
+# Hyperparameter 
