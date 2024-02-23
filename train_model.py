658f24789b637c32c2e90958fe40ccb49064afde
Coy Morris <crm058@uark.edu>
2024-02-23 10:30:41 -0600

    Created tune script to find ideal hyperparameters for model.

--- train_model.py
+++ train_model.py
@@ -2,7 +2,9 @@
 train_model.py
 ~~~~~~~~~~~~
 
-A module to train YOLO models on UARK hpc. Functionality includes (as far as I've thought) 
+A module to train YOLO models on UARK hpc. Functionality includes (as far as I've thought)....
+TO DO: create a tune_hyperparamter script that outputs
+
 
 """
 # %%
@@ -10,6 +12,7 @@ A module to train YOLO models on UARK hpc. Functionality includes (as far as I'v
 import os
 import torch
 from ultralytics import YOLO
+import typing
 
 # Specifying cuda GPU memory allocation. Required in google colab to deal with memory overrun, using as safeguard for HPC
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:256'
@@ -22,4 +25,6 @@ else:
 print(f"Using {device_name} for training.")
 
 # %%
-# Hyperparameter 
+# Hyperparameter selection 
+def choose_hyperparameters() -> None:
+    pass
---
+++ tune_model.py
@@ -0,0 +1,9 @@
+"""
+tune_model.py
+~~~~~~~~~~~~
+
+A module to tune model hyperparameters on HPC at UARK using Tuner library in ultralytics. Outputs .csv file of ideal hyperparameters
+found during the tuning process. Thinking about inputing this csv into the train script to automatically  
+
+
+"""
